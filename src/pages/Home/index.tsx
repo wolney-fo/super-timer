a5371a8923b9e73f7ad3be7d2481c7dd59ff6a83
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2025-02-02 15:52:43 -0300

    feat: create Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I will work on</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Name your project"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+            <option value="Project 4" />
+          </datalist>
+
+          <label htmlFor="minuetsAmount">for</label>
+          <MinutesAmountInput
+            type="number"
+            id="minuetsAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
